880aa195e64f80de87510409ca958dc6bd46be10
Diego Aparicio <d.aparicio@unistra.fr>
2025-10-04 16:07:23 +0200

    Autosize et parsing d'options style commande linux

--- src/main.c
+++ src/main.c
@@ -2,20 +2,33 @@
 #include "game.h"
 #include "timer.h"
 #include "utils.h"
+#include <stdio.h>
 
-#define MAX_WIDTH 999
-#define MAX_HEIGHT 999
+#define MAX_WIDTH get_terminal_width()/4 - 1
+#define MAX_HEIGHT get_terminal_height()/2 - 2
 #define MAX_TIME_LIMIT 3599
 
 int main(int argc, char* argv[]) {
-    int width = 5, height = 5, time_limit = -1;
+    int width = MAX_WIDTH;
+    int height = MAX_HEIGHT;
+    int time_limit = -1;
 
-    if (argc >= 3) {
-        width = clamp_int(atoi(argv[1]), 1, MAX_WIDTH);
-        height = clamp_int(atoi(argv[2]), 1, MAX_HEIGHT);
-    }
-    if (argc == 4) {
-        time_limit = clamp_int(atoi(argv[3]), 0, MAX_TIME_LIMIT);
+    int opt;
+    while ((opt = getopt(argc, argv, "w:h:t:")) != -1) {
+        switch (opt) {
+            case 'w':
+                width = clamp_int(atoi(optarg), 1, width);
+                break;
+            case 'h':
+                height = clamp_int(atoi(optarg), 1, height);
+                break;
+            case 't':
+                time_limit = clamp_int(atoi(optarg), 0, MAX_TIME_LIMIT);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-w width] [-h height] [-t seconds]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
     }
 
     game_start(width, height, time_limit);
